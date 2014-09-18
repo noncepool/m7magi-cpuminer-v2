9cba04df29e47e52c228eefa1e93f1c6bb94b50d
noncepool <noncepool@gmail.com>
2014-09-18 09:06:21 -0400

    init

---
+++ m7mhash.c
@@ -0,0 +1,232 @@
+
+#include "cpuminer-config.h"
+#include "miner.h"
+
+#include <gmp.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sph_sha2.h>
+#include <sph_keccak.h>
+#include <sph_haval.h>
+#include <sph_tiger.h>
+#include <sph_whirlpool.h>
+#include <sph_ripemd.h>
+
+static void mpz_set_uint256(mpz_t r, uint8_t *u)
+{
+    mpz_import(r, 32 / sizeof(unsigned long), -1, sizeof(unsigned long), -1, 0, u);
+}
+
+static void mpz_get_uint256(mpz_t r, uint8_t *u)
+{
+    u=0;
+    mpz_export(u, 0, -1, sizeof(unsigned long), -1, 0, r);
+}
+
+static void mpz_set_uint512(mpz_t r, uint8_t *u)
+{
+    mpz_import(r, 64 / sizeof(unsigned long), -1, sizeof(unsigned long), -1, 0, u);
+}
+
+static void set_one_if_zero(uint8_t *hash512) {
+    for (int i = 0; i < 32; i++) {
+        if (hash512[i] != 0) {
+            return;
+        }
+    }
+    hash512[0] = 1;
+}
+
+static bool fulltest_m7hash(const uint32_t *hash32, const uint32_t *target32)
+{
+    int i;
+    bool rc = true;
+
+    const unsigned char *hash = (const unsigned char *)hash32;
+    const unsigned char *target = (const unsigned char *)target32;
+    for (i = 31; i >= 0; i--) {
+        if (hash[i] != target[i]) {
+            rc = hash[i] < target[i];
+            break;
+        }
+    }
+
+    return rc;
+}
+
+#define NM7M 5
+#define M7_MIDSTATE_LEN 76
+int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
+    uint64_t max_nonce, unsigned long *hashes_done)
+{
+    uint32_t data[32] __attribute__((aligned(128)));
+    uint32_t *data_p64 = data + (M7_MIDSTATE_LEN / sizeof(data[0]));
+    uint32_t hash[8] __attribute__((aligned(32)));
+    uint8_t bhash[7][64] __attribute__((aligned(32)));
+    uint32_t n = pdata[19] - 1;
+    const uint32_t first_nonce = pdata[19];
+    char data_str[161], hash_str[65], target_str[65];
+    uint8_t *bdata = 0;
+    mpz_t bns[8];
+    int rc = 0;
+
+    mpz_t product;
+    mpz_init(product);
+
+    for(int i=0; i < 8; i++){
+        mpz_init(bns[i]);
+    }
+
+    memcpy(data, pdata, 80);
+
+    sph_sha256_context       ctx_final_sha256;
+
+    sph_sha256_context       ctx_sha256;
+    sph_sha512_context       ctx_sha512;
+    sph_keccak512_context    ctx_keccak;
+    sph_whirlpool_context    ctx_whirlpool;
+    sph_haval256_5_context   ctx_haval;
+    sph_tiger_context        ctx_tiger;
+    sph_ripemd160_context    ctx_ripemd;
+
+    sph_sha256_init(&ctx_sha256);
+    sph_sha256 (&ctx_sha256, data, M7_MIDSTATE_LEN);
+    
+    sph_sha512_init(&ctx_sha512);
+    sph_sha512 (&ctx_sha512, data, M7_MIDSTATE_LEN);
+    
+    sph_keccak512_init(&ctx_keccak);
+    sph_keccak512 (&ctx_keccak, data, M7_MIDSTATE_LEN);
+
+    sph_whirlpool_init(&ctx_whirlpool);
+    sph_whirlpool (&ctx_whirlpool, data, M7_MIDSTATE_LEN);
+    
+    sph_haval256_5_init(&ctx_haval);
+    sph_haval256_5 (&ctx_haval, data, M7_MIDSTATE_LEN);
+
+    sph_tiger_init(&ctx_tiger);
+    sph_tiger (&ctx_tiger, data, M7_MIDSTATE_LEN);
+
+    sph_ripemd160_init(&ctx_ripemd);
+    sph_ripemd160 (&ctx_ripemd, data, M7_MIDSTATE_LEN);
+
+    sph_sha256_context       ctx2_sha256;
+    sph_sha512_context       ctx2_sha512;
+    sph_keccak512_context    ctx2_keccak;
+    sph_whirlpool_context    ctx2_whirlpool;
+    sph_haval256_5_context   ctx2_haval;
+    sph_tiger_context        ctx2_tiger;
+    sph_ripemd160_context    ctx2_ripemd;
+
+    do {
+        data[19] = ++n;
+
+        memset(bhash, 0, 7 * 64);
+
+        ctx2_sha256 = ctx_sha256;
+        sph_sha256 (&ctx2_sha256, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_sha256_close(&ctx2_sha256, (void*)(bhash[0]));
+
+        ctx2_sha512 = ctx_sha512;
+        sph_sha512 (&ctx2_sha512, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_sha512_close(&ctx2_sha512, (void*)(bhash[1]));
+        
+        ctx2_keccak = ctx_keccak;
+        sph_keccak512 (&ctx2_keccak, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_keccak512_close(&ctx2_keccak, (void*)(bhash[2]));
+
+        ctx2_whirlpool = ctx_whirlpool;
+        sph_whirlpool (&ctx2_whirlpool, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_whirlpool_close(&ctx2_whirlpool, (void*)(bhash[3]));
+        
+        ctx2_haval = ctx_haval;
+        sph_haval256_5 (&ctx2_haval, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_haval256_5_close(&ctx2_haval, (void*)(bhash[4]));
+
+        ctx2_tiger = ctx_tiger;
+        sph_tiger (&ctx2_tiger, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_tiger_close(&ctx2_tiger, (void*)(bhash[5]));
+
+        ctx2_ripemd = ctx_ripemd;
+        sph_ripemd160 (&ctx2_ripemd, data_p64, 80 - M7_MIDSTATE_LEN);
+        sph_ripemd160_close(&ctx2_ripemd, (void*)(bhash[6]));
+
+        for(int i=0; i < 7; i++){
+            set_one_if_zero(bhash[i]);
+            mpz_set_uint512(bns[i],bhash[i]);
+        }
+
+        mpz_set_ui(bns[7],0);
+
+        for(int i=0; i < 7; i++){
+	        mpz_add(bns[7], bns[7], bns[i]);
+        }
+
+        mpz_set_ui(product,1);
+
+        for(int i=0; i < 8; i++){
+            mpz_mul(product,product,bns[i]);
+        }
+
+        mpz_pow_ui(product, product, 2); 
+
+        int bytes = mpz_sizeinbase(product, 256);
+        bdata = (uint8_t *)realloc(bdata, bytes);
+        mpz_export((void *)bdata, NULL, -1, 1, 0, 0, product);
+
+        sph_sha256_init(&ctx_final_sha256);
+        sph_sha256 (&ctx_final_sha256, bdata, bytes);
+        sph_sha256_close(&ctx_final_sha256, (void*)(hash));
+
+        for(int i=0; i < NM7M; i++){
+            mpz_set_uint256(bns[0], (void*)(hash));
+            mpz_add(bns[7], bns[7], bns[0]);
+
+            mpz_mul(product, product, bns[7]);
+            mpz_cdiv_q (product, product, bns[0]);
+            if (mpz_sgn(product) <= 0) mpz_set_ui(product,1);
+
+            bytes = mpz_sizeinbase(product, 256);
+            bdata = (uint8_t *)realloc(bdata, bytes);
+            mpz_export(bdata, NULL, -1, 1, 0, 0, product);
+
+            sph_sha256_init(&ctx_final_sha256);
+            sph_sha256 (&ctx_final_sha256, bdata, bytes);
+            sph_sha256_close(&ctx_final_sha256, (void*)(hash));
+        }
+
+        rc = fulltest_m7hash(hash, ptarget);
+        if (rc) {
+            if (opt_debug) {
+                bin2hex(hash_str, (unsigned char *)hash, 32);
+                bin2hex(target_str, (unsigned char *)ptarget, 32);
+                bin2hex(data_str, (unsigned char *)data, 80);
+                applog(LOG_DEBUG, "DEBUG: [%d thread] Found share!\ndata   %s\nhash   %s\ntarget %s", thr_id, 
+                    data_str,
+                    hash_str,
+                    target_str);
+            }
+
+            pdata[19] = data[19];
+
+            goto out;
+        }
+    } while (n < max_nonce && !work_restart[thr_id].restart);
+
+    pdata[19] = n;
+
+out:
+    for(int i=0; i < 8; i++){
+        mpz_clear(bns[i]);
+    }
+    mpz_clear(product);
+    free(bdata);
+
+    *hashes_done = n - first_nonce + 1;
+    return rc;
+}
+
+
+
