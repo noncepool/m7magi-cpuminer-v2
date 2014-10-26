b1f95b96b81363f64ee5ea933e92b20d6a222566
noncepool <noncepool@gmail.com>
2014-10-26 06:55:55 -0400

    update to m7m-hash-v2

--- m7mhash.c
+++ m7mhash.c
@@ -1,12 +1,14 @@
 
 #include "cpuminer-config.h"
 #include "miner.h"
+#include "magimath.h"
 
 #include <gmp.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <float.h>
+#include <math.h>
 #include <sph_sha2.h>
 #include <sph_keccak.h>
 #include <sph_haval.h>
@@ -56,7 +58,11 @@ static bool fulltest_m7hash(const uint32_t *hash32, const uint32_t *target32)
     return rc;
 }
 
+#define BITS_PER_DIGIT 3.32192809488736234787
+#define EPS (DBL_EPSILON)
+
 #define NM7M 5
+#define SW_DIVS 5
 #define M7_MIDSTATE_LEN 76
 int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
     uint64_t max_nonce, unsigned long *hashes_done)
@@ -71,6 +77,7 @@ int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
     uint8_t *bdata = 0;
     mpz_t bns[8];
     int rc = 0;
+    int bytes, nnNonce2;
 
     mpz_t product;
     mpz_init(product);
@@ -122,7 +129,7 @@ int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 
     do {
         data[19] = ++n;
-
+        nnNonce2 = (int)(data[19]/2);
         memset(bhash, 0, 7 * 64);
 
         ctx2_sha256 = ctx_sha256;
@@ -172,7 +179,7 @@ int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 
         mpz_pow_ui(product, product, 2); 
 
-        int bytes = mpz_sizeinbase(product, 256);
+        bytes = mpz_sizeinbase(product, 256);
         bdata = (uint8_t *)realloc(bdata, bytes);
         mpz_export((void *)bdata, NULL, -1, 1, 0, 0, product);
 
@@ -180,22 +187,116 @@ int scanhash_m7m_hash(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
         sph_sha256 (&ctx_final_sha256, bdata, bytes);
         sph_sha256_close(&ctx_final_sha256, (void*)(hash));
 
+        int digits=(int)((sqrt((double)(nnNonce2))*(1.+EPS))/9000+75);
+        int iterations=20;
+        mpf_set_default_prec((long int)(digits*BITS_PER_DIGIT+16));
+
+        mpz_t magipi;
+        mpz_t magisw;
+        mpf_t magifpi;
+        mpf_t mpa1, mpb1, mpt1, mpp1;
+        mpf_t mpa2, mpb2, mpt2, mpp2;
+        mpf_t mpsft;
+
+        mpz_init(magipi);
+        mpz_init(magisw);
+        mpf_init(magifpi);
+        mpf_init(mpsft);
+        mpf_init(mpa1);
+        mpf_init(mpb1);
+        mpf_init(mpt1);
+        mpf_init(mpp1);
+
+        mpf_init(mpa2);
+        mpf_init(mpb2);
+        mpf_init(mpt2);
+        mpf_init(mpp2);
+
+        uint32_t usw_;
+        usw_ = sw_(nnNonce2, SW_DIVS);
+        if (usw_ < 1) usw_ = 1;
+        mpz_set_ui(magisw, usw_);
+        uint32_t mpzscale=mpz_size(magisw);
+
         for(int i=0; i < NM7M; i++){
+            if (mpzscale > 1000) {
+                mpzscale = 1000;
+            }
+            else if (mpzscale < 1) {
+                mpzscale = 1;
+            }
+
+            mpf_set_ui(mpa1, 1);
+            mpf_set_ui(mpb1, 2);
+            mpf_set_d(mpt1, 0.25*mpzscale);
+            mpf_set_ui(mpp1, 1);
+            mpf_sqrt(mpb1, mpb1);
+            mpf_ui_div(mpb1, 1, mpb1);
+            mpf_set_ui(mpsft, 10);
+
+            for(int j=0; j <= iterations; j++){
+	            mpf_add(mpa2, mpa1,  mpb1);
+            	mpf_div_ui(mpa2, mpa2, 2);
+            	mpf_mul(mpb2, mpa1, mpb1);
+            	mpf_abs(mpb2, mpb2);
+            	mpf_sqrt(mpb2, mpb2);
+            	mpf_sub(mpt2, mpa1, mpa2);
+            	mpf_abs(mpt2, mpt2);
+            	mpf_sqrt(mpt2, mpt2);
+            	mpf_mul(mpt2, mpt2, mpp1);
+            	mpf_sub(mpt2, mpt1, mpt2);
+            	mpf_mul_ui(mpp2, mpp1, 2);
+            	mpf_swap(mpa1, mpa2);
+            	mpf_swap(mpb1, mpb2);
+            	mpf_swap(mpt1, mpt2);
+            	mpf_swap(mpp1, mpp2);
+            }
+
+            mpf_add(magifpi, mpa1, mpb1);
+            mpf_pow_ui(magifpi, magifpi, 2);
+            mpf_div_ui(magifpi, magifpi, 4);
+            mpf_abs(mpt1, mpt1);
+            mpf_div(magifpi, magifpi, mpt1);
+
+            mpf_pow_ui(mpsft, mpsft, digits/2);
+            mpf_mul(magifpi, magifpi, mpsft);
+
+            mpz_set_f(magipi, magifpi);
+
+            mpz_add(product,product,magipi);
+            mpz_add(product,product,magisw);
+
             mpz_set_uint256(bns[0], (void*)(hash));
             mpz_add(bns[7], bns[7], bns[0]);
 
-            mpz_mul(product, product, bns[7]);
+            mpz_mul(product,product,bns[7]);
             mpz_cdiv_q (product, product, bns[0]);
             if (mpz_sgn(product) <= 0) mpz_set_ui(product,1);
 
             bytes = mpz_sizeinbase(product, 256);
+            mpzscale=bytes;
             bdata = (uint8_t *)realloc(bdata, bytes);
             mpz_export(bdata, NULL, -1, 1, 0, 0, product);
 
             sph_sha256_init(&ctx_final_sha256);
             sph_sha256 (&ctx_final_sha256, bdata, bytes);
             sph_sha256_close(&ctx_final_sha256, (void*)(hash));
+
         }
+
+        mpz_clear(magipi);
+        mpz_clear(magisw);
+        mpf_clear(magifpi);
+        mpf_clear(mpsft);
+        mpf_clear(mpa1);
+        mpf_clear(mpb1);
+        mpf_clear(mpt1);
+        mpf_clear(mpp1);
+
+        mpf_clear(mpa2);
+        mpf_clear(mpb2);
+        mpf_clear(mpt2);
+        mpf_clear(mpp2);
 
         rc = fulltest_m7hash(hash, ptarget);
         if (rc) {
